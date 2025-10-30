88676baec49e72a6656db805285f94704e10cc59
OverGoro <Yurigorodskiy@yandex.ru>
2025-10-30 04:51:24 +0300

    removed links form test resources

--- code/service/message-service/src/test/resources/db/connection-init.sql
+++ code/service/message-service/src/test/resources/db/connection-init.sql
@@ -1,1 +1,31 @@
-/home/yurik/GitHub/ConnectionProject/code/src/test/resources/db/connection-init.sql
+create table if not exists processing.connection_scheme(
+    uid uuid primary key, 
+    client_uid uuid not null references core.client(uid) on delete cascade,
+    scheme_json jsonb not null
+);
+
+
+create table if not exists processing.buffer(
+    uid uuid primary key, 
+    device_uid uuid not null references core.device(uid) on delete cascade,
+    max_messages_number integer not null CHECK (max_messages_number > 0),
+    max_message_size integer not null CHECK ( max_message_size > 0 ),
+    message_prototype varchar
+);
+
+
+create table if not exists processing.connection_scheme_buffer(
+    uid uuid primary key,
+    scheme_uid uuid not null references processing.connection_scheme(uid) on delete cascade,
+    buffer_uid uuid not null references processing.buffer(uid) on delete cascade
+);
+
+create table if not exists processing.message(
+    uid uuid primary key, 
+    buffer_uid uuid not null references processing.buffer(uid) on delete cascade,
+    content jsonb not null, 
+    content_type varchar not null, -- incomming/outcomming
+    created_at TIMESTAMP WITH TIME zone not null
+);
+
+
